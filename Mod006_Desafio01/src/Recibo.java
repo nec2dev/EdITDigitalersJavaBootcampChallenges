d8509a48a0ed1fa3dac61a76e1d1bb2b0545bfba
Nahuel <71109200+nec2dev@users.noreply.github.com>
2022-06-18 14:52:48 -0300

    Add files via upload

---
+++ Mod006_Desafio01/src/Recibo.java
@@ -0,0 +1,40 @@
+
+public class Recibo {
+	//Atributos
+	private String tipoElemento;
+	private float valor;
+	private int totalDiario;
+	
+	//Metodos
+	public Recibo(String tipoElemento, float valor, int totalDiario) {
+		super();
+		this.tipoElemento = tipoElemento;
+		this.valor = valor;
+		this.totalDiario = totalDiario;
+	}
+
+	public String getTipoElemento() {
+		return tipoElemento;
+	}
+
+	public void setTipoElemento(String tipoElemento) {
+		this.tipoElemento = tipoElemento;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public int getTotalDiario() {
+		return totalDiario;
+	}
+
+	public void setTotalDiario(int totalDiario) {
+		this.totalDiario = totalDiario;
+	}
+	
+}
