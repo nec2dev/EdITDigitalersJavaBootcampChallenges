b393656b06c1f445511f629bd11995ceb347b602
Nahuel <71109200+nec2dev@users.noreply.github.com>
2022-06-18 15:15:38 -0300

    Add files via upload

---
+++ Mod007_Desafio01/src/Elemento.java
@@ -0,0 +1,53 @@
+
+public class Elemento {
+	//Atributos
+	private String tipoElemento;
+	private float ancho;
+	private float alto;
+	private float peso;
+	
+	//Metodos
+	public Elemento() {
+		
+	}
+	public Elemento(String tipoElemento, float ancho, float alto, float peso) {
+		super();
+		this.tipoElemento = tipoElemento;
+		this.ancho = ancho;
+		this.alto = alto;
+		this.peso = peso;
+	}
+
+	public String getTipoElemento() {
+		return tipoElemento;
+	}
+
+	public void setTipoElemento(String tipoElemento) {
+		this.tipoElemento = tipoElemento;
+	}
+
+	public float getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(float ancho) {
+		this.ancho = ancho;
+	}
+
+	public float getAlto() {
+		return alto;
+	}
+
+	public void setAlto(float alto) {
+		this.alto = alto;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+}
